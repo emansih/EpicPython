d895dcc432168554a0b6f3f071846918afbefc96
yiyhuang <oliviahyy1126@gmail.com>
2021-04-09 17:02:01 +1000

    make slight change to 3 buttons on welcome page

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signup/donor')
+def signup_donor():
+    return render_template('signup_donor.html')\
+
+@app.route('/login')
+def login():
+    return render_template('login.html')

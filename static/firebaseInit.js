e73daade9b3f2e580a2556fcb72e25e22a78dd3a
emansih <danielquah148@yahoo.com>
2021-04-10 21:37:48 +1000

    Add firebase auth

---
+++ static/firebaseInit.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAE8MoCIIsT1cS5o5tktvbz5nCtI4NkmjA",
+    authDomain: "epicpython-d990d.firebaseapp.com",
+    projectId: "epicpython-d990d",
+    storageBucket: "epicpython-d990d.appspot.com",
+    messagingSenderId: "520305167017",
+    appId: "1:520305167017:web:fbfc8b8ed561e0bb66f7b8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
